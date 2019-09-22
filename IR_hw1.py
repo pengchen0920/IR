62338d875fe18c523aa956db9df6b5f674ca442d
彭晨 <37538060+pengchen0920@users.noreply.github.com>
2019-09-22 20:08:12 +0800

    Add files via upload

---
+++ IR_hw1.py
@@ -0,0 +1,43 @@
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+# 將 https://ceiba.ntu.edu.tw/course/35d27d/content/28.txt 中的文字轉存為 IR_hw1_input.txt
+with open("IR_hw1_input.txt") as fp: 
+    lines = fp.read().splitlines()
+
+text = ""
+text = "".join(lines)
+
+
+# (1)Tokenization
+# 切割字的同時，清除標點符號
+tokenizer = RegexpTokenizer(r'\w+')  
+tokens = tokenizer.tokenize(text)
+
+
+# (2)Lowercasing everything：
+tokens_lower = [w.lower() for w in tokens]
+
+
+# (3)Stemming using Porter’s algorithm
+porter_stemmer = PorterStemmer()
+tokens_stemmed = [porter_stemmer.stem(w) for w in tokens_lower]
+
+
+# (4)Stopwords removal
+stop_words = set(stopwords.words('english'))
+tokens_filtered = []   
+for w in tokens_stemmed: 
+    if w not in stop_words: 
+        tokens_filtered.append(w) 
+
+# final output
+with open('IR_hw1_output.txt', 'w') as file:
+    for tokens in tokens_filtered:
+        file.write('%s\n' % tokens)
+
+
+
+
+
